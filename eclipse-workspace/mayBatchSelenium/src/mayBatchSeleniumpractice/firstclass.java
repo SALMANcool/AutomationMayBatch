bba4ed16b0342fe5fb7976001e4e9642d463599e
salman irshad <salmanirshad787@gmail.com>
2021-05-30 18:30:07 +0530

    firstclass

---
+++ eclipse-workspace/mayBatchSelenium/src/mayBatchSeleniumpractice/firstclass.java
@@ -0,0 +1,13 @@
+package mayBatchSeleniumpractice;
+
+public class firstclass {
+	
+public void m1()
+{
+	System.out.println("salman cool");
+}
+public static void main(String[] args) {
+	firstclass fc=new firstclass();
+	fc.m1();
+}
+}
